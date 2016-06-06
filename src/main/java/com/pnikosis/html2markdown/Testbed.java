4b993960ead111e46a93166833481abf85ff0b31
Nicolás Hormazábal <nicolas.hormazabal@schibsted.com>
2016-06-06 08:58:33 +0200

    idnentation changes

--- src/main/java/com/pnikosis/html2markdown/Testbed.java
+++ src/main/java/com/pnikosis/html2markdown/Testbed.java
@@ -5,24 +5,24 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class Testbed {
-    public static void main(String[] args) {
-        URL url;
-        try {
-            url = new URL("http://jsoup.org/");
-            String parsedText = HTML2Md.convert(url, 30000);
-            System.out.println(parsedText);
+  public static void main(String[] args) {
+    URL url;
+    try {
+      url = new URL("http://jsoup.org/");
+      String parsedText = HTML2Md.convert(url, 30000);
+      System.out.println(parsedText);
 
-            // test parse local html file
+      // test parse local html file
 //            String pathFile = "test.html";
 //            File f = new File(pathFile);
 //            String parsedFileText = HTML2Md.convertFile(f, "gbk");
 //            System.out.println(parsedFileText);
 
-            System.out.println("done");
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+      System.out.println("done");
+    } catch (MalformedURLException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+  }
 }

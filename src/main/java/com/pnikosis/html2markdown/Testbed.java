0e8bc9896a05d48ec220b7135e823727b5f992e4
Nicolás Hormazábal <nicolas.hormazabal@schibsted.com>
2016-06-06 08:33:13 +0200

    setup gradle

---
+++ src/main/java/com/pnikosis/html2markdown/Testbed.java
@@ -0,0 +1,28 @@
+package com.pnikosis.html2markdown;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Testbed {
+    public static void main(String[] args) {
+        URL url;
+        try {
+            url = new URL("http://jsoup.org/");
+            String parsedText = HTML2Md.convert(url, 30000);
+            System.out.println(parsedText);
+
+            // test parse local html file
+//            String pathFile = "test.html";
+//            File f = new File(pathFile);
+//            String parsedFileText = HTML2Md.convertFile(f, "gbk");
+//            System.out.println(parsedFileText);
+
+            System.out.println("done");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
